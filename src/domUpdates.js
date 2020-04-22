2a2511d357c2f1f34dafd975583372f09ea49dc5
Joe Haefling <josephhaefling@gmail.com>
2020-04-21 20:55:51 -0600

    Style the user and manger pages

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,46 +1,41 @@
 import $ from 'jQuery'
-
 const domUpdates = {
-
   displayManagerInfo(manager) {
     this.displayVacancies(manager)
   },
-
  displayAppropriatePage(userType) {
     $('.login-container').toggle('hide')
     $(`.${userType}-page`).toggle('hide')
     $(`.${userType}-page`).css('display', 'grid')
   },
-
   showIDError() {
     $('#ID-label').text('Invalid Name');
     $('#ID-label').css('color','Red');
     $('#userID').css('box-shadow', '0px 0px 7px 2px red')
   },
-
   showPasswordError() {
     $('#password-label').text('Invalid Password')
     $('#password-label').css('color', 'Red')
     $('#password').css('box-shadow', '0px 0px 7px 2px red')
   },
-
   displayManagerInfo(manager, roomRepo, bookingRepo) {
+    $('#body').css('background-color','rgba(32, 32, 32)')
     $('.vacancies-text').text(`There are currently ${manager.getVacantRooms(roomRepo, bookingRepo).length} available rooms.`)
     $('.revenue-text').text(`Todays revenue is $${manager.getTotalRevenue(bookingRepo)}.`)
     $('.available-text').text(`${manager.getPercentageOfRoomsAvailable(roomRepo, bookingRepo)}% of our rooms are available.`)
   },
-
   displayUserInfo(user) {
-      $('#body').css('background-image','url(../images/creepyGraveYard.jpg)')
+      // $('#body').css('background-image','none')
+      $('#body').css('background-color','rgba(32, 32, 32, .9)')
       $('.user-header').text(`Welcome back ${user.userName.split(' ')[0]}`)
       $('.total-spent-text').text(`You have spent $${user.getTotalSpent()} with us.`)
       $('.current-bookings-text').text(`You have ${user.getCurrentBookings().length} bookings for today!`)
       $('.future-bookings-text').text(`You have ${user.getFutureBookings().length} upcoming bookings.`)
       $('.past-bookings-text').text(`You have ${user.getPastBookings().length} previous bookings.`)
   },
-
   displayAvailableRooms(manager, selectedDate, roomRepo, bookingRepo) {
     let date = selectedDate.toString().split(' ').slice(0, 4).join(' ')
+    $('.customer-page').toggle('hide')
     $('#body').css('background-image', 'none')
     $('#body').css('background-color','black')
     $('.see-rooms-btn').parent().parent().toggle('hide')
@@ -48,13 +43,13 @@ const domUpdates = {
     $('.available-rooms').text(`Here are the available rooms for ${date}`)
     this.createRoomCards(manager, roomRepo, bookingRepo)
   },
-
     createRoomCards(manager, roomRepo, bookingRepo) {
       let vacantRooms = manager.getVacantRooms(roomRepo, bookingRepo)
       vacantRooms.length > 0 ? vacantRooms.forEach(room   => {
+        Math.floor(Math.random() * (7 - 1)) + 1
         $('.room-container').prepend(
           `<button class="room-section" id="${room.number}">
-           <div class="room-button room-image" id="${room.number}"></div>
+           <div class="room-button room-image${Math.floor(Math.random() * (7 - 1)) + 1}" id="${room.number}"></div>
            <span class="room-button room-number" id="${room.number}">Room#${room.number}</span>
            <span class="room-buttonroom-type" id="${room.number}">Room Type:${room.roomType}</span>
            <span class="room-button number-beds" id="${room.number}">Beds:${room.numBeds}</span>
@@ -63,7 +58,6 @@ const domUpdates = {
         )
       }) : this.saySorry(manager)
     },
-
     filterRooms(event, availableRooms) {
       let inputValue = $('#room-type-drop-down').val()
       availableRooms.forEach(room => {
@@ -72,7 +66,6 @@ const domUpdates = {
         }
       });
     },
-
     saySorry(manager) {
       let date =manager.currentDate.toString().split(' ').slice(0, 4).join(' ');
       $('.room-container').prepend(
@@ -80,7 +73,6 @@ const domUpdates = {
          <span class="room-button bed-size" id="$sorry-text">We are sorry but we don't have any vacancies on ${date}</span>
          </section>`
       )},
-
       displayThankYou() {
         $('.room-container').toggle('hide')
         $(`<section class="room-section" id="thanks-page">
@@ -95,5 +87,4 @@ const domUpdates = {
       })
     }
 }
-
 export default domUpdates
--- src/index.js
+++ src/index.js
@@ -15,27 +15,27 @@ import './images/creepyGraveYard.jpg';
 import './images/revenue.jpg';
 import './images/vacancy.jpg';
 import './images/theBlackLodge.jpg';
+import './images/guilitine.jpg';
 import './images/creepyRoom1.jpg';
+import './images/creepyRoom2.jpg';
+import './images/creepyRoom3.jpg';
+import './images/creepyRoom4.jpg';
+import './images/creepyRoom5.jpg';
+import './images/creepyRoom6.jpg';
 let userLoginRepo = []
 let userRepo = []
 let roomRepo = []
 let bookingRepo = []
 let loggedInUser;
-let todaysDate = '2020/02/05'
+let todaysDate = new Date(Date())
 let skeletInn;
-
 $('.large-btn').click(function() {
   loginUser();
 })
-// $('.search-button').click(function() {
-//   searchRooms();
-// })
-
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
 .then(response => response.json())
 .then(data => createUserRepo(data))
 .catch(err => console.error(err))
-
 setTimeout(function fetchData() {
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
 .then(response => response.json())
@@ -46,20 +46,28 @@ fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
 .then(data => createBookings(data))
 .catch(data => console.error())
 })
-
-
 let createUserRepo = (userData) => {
   userData.users.forEach(user => {
     userLoginRepo.push(user)
+    convertDate()
   })
 }
-
+let convertDate = () => {
+  let newDate = []
+  let date = todaysDate.toISOString().split('').slice(0, 10)
+  let formattedDated = date.forEach(element => {
+  if (element === '-') {
+    element = '/'
+  }
+  newDate.push(element)
+})
+  return newDate.join('');
+}
 let createRooms = (roomData) => {
   roomData.rooms.forEach(room => {
   roomRepo.push(room)
   })
 }
-
 let createBookings = (bookingData) => {
   bookingData.bookings.forEach(booking => {
     let newBooking = new Booking(booking)
@@ -68,20 +76,17 @@ let createBookings = (bookingData) => {
   createUsers(userLoginRepo)
   createHotel('2020/02/05')
 }
-
 let createUsers = (userLoginRepo) => {
   userLoginRepo.forEach(user => {
-    let guest = new User(user, bookingRepo, todaysDate, roomRepo)
+    let guest = new User(user, bookingRepo, convertDate(), roomRepo)
     userRepo.push(guest)
   })
 }
-
 let createHotel = (date) => {
   skeletInn = new Hotel(date, roomRepo)
   createManager(todaysDate, skeletInn, roomRepo)
   getAvailableRooms(skeletInn)
 }
-
 let createManager = (currentDate, hotel, roomRepo) => {
   let lucyPhurr = new Manager(currentDate, hotel, roomRepo)
   domUpdates.displayManagerInfo(lucyPhurr, roomRepo, bookingRepo)
@@ -89,12 +94,10 @@ let createManager = (currentDate, hotel, roomRepo) => {
     getAvailableRoomInfo(roomRepo, bookingRepo)
   })
 }
-
 let loginUser = () => {
   let userID = $('#userID').val();
   userID === 'manager' ? verifyPassword('manager') : verifyUser(userID)
 }
-
 let verifyUser = (userID) => {
   let currentUser = userRepo.find(user => userID === `customer${user.userID}`)
   let loginType = userID.replace(/[0-9]/g, '');
@@ -102,30 +105,24 @@ let verifyUser = (userID) => {
   domUpdates.displayUserInfo(currentUser)
   loggedInUser = currentUser
 }
-
 let verifyPassword = (userType) => {
   let passWord = $('#password').val()
   passWord === 'overlook2020' ? domUpdates.displayAppropriatePage(userType) : domUpdates.showPasswordError()
 }
-
 let getAvailableRoomInfo = (roomRepo, bookingRepo) => {
   let userRequest = new Manager($('.calander').val(), skeletInn, roomRepo)
-  let formattedDate = [$('.calander').val().slice(-4), $('.calander').val().slice(0,5)].join('/');
+  let formattedDate = [$('.calander').val().slice(-4), $('.calander').val().slice(0,5)].join('/')
   let userDate = new Date($('.calander').val())
   domUpdates.displayAvailableRooms(userRequest, userDate, roomRepo, bookingRepo)
-  searchRooms(userRequest, todaysDate, roomRepo, bookingRepo)
-  createNewBooking(userRequest, todaysDate, roomRepo, bookingRepo)
+  searchRooms(userRequest, convertDate(), roomRepo, bookingRepo)
+  createNewBooking(userRequest, convertDate(), roomRepo, bookingRepo)
 }
-
 let searchRooms = (manager, todaysDate, roomRepo, bookingRepo) => {
   $('.search-button').click(() => {
     let availableRooms = manager.getVacantRooms(roomRepo, bookingRepo)
     availableRooms.length > 0 ? domUpdates.filterRooms($(event.target).parent(), availableRooms) : domUpdates.saySorry()
-
 })
 }
-
-
 let createNewBooking = (manager, todaysDate, roomRepo, bookingRepo, currentUser) => {
   $('.room-button').click(() => {
     let roomsAvailable = manager.getVacantRooms(roomRepo, bookingRepo)
@@ -136,7 +133,6 @@ let createNewBooking = (manager, todaysDate, roomRepo, bookingRepo, currentUser)
     postBooking(currentUser, booking[0], date)
 })
 }
-
 let getFormattedDate = (currentUser) => {
   let newDate = []
   let date = currentUser.todaysDate.toISOString().split('').slice(0, 10)
@@ -148,7 +144,6 @@ let getFormattedDate = (currentUser) => {
   })
   return newDate
 }
-
 function postBooking(currentUser, room, date) {
      fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
       method: 'POST',
@@ -167,7 +162,6 @@ function postBooking(currentUser, room, date) {
        refreshBookingInfo()
        domUpdates.goBackToMain()
 }
-
 let refreshBookingInfo = () => {
   fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
   .then(response => response.json())
